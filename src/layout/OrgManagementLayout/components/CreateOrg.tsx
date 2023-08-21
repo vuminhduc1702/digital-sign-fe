9ce90cca5ecf62fd6574b47515ab2ab0e9a3b862
datpt34 <datpt034@gmail.com>
2023-08-21 14:53:41 +0700

    fix org

--- src/layout/OrgManagementLayout/components/CreateOrg.tsx
+++ src/layout/OrgManagementLayout/components/CreateOrg.tsx
@@ -1,39 +1,42 @@
 import * as z from 'zod'
 import { useTranslation } from 'react-i18next'
-import { useState } from 'react'
 
 import { Button } from '~/components/Button'
-import { Form, FormDrawer, InputField, TextAreaField } from '~/components/Form'
-import { ComboBoxSelectOrg } from '~/layout/MainLayout/components'
+import {Form, FormDrawer, InputField, SelectDropdown, TextAreaField} from '~/components/Form'
 import { type CreateOrgDTO, useCreateOrg } from '../api'
-import { descSchema, nameSchema } from '~/utils/schemaValidation'
-import { useDefaultCombobox } from '~/utils/hooks'
+import {descSchema, nameSchema, selectOptionSchema} from '~/utils/schemaValidation'
 import storage from '~/utils/storage'
-
-import { type OrgMapType } from './OrgManageSidebar'
+import {OrgList} from "~/layout/MainLayout/types";
+import {queryClient} from "~/lib/react-query.ts";
+import {flattenData} from "~/utils/misc.ts";
+import {useDefaultCombobox} from "~/utils/hooks.ts";
 
 import { PlusIcon } from '~/components/SVGIcons'
 import btnSubmitIcon from '~/assets/icons/btn-submit.svg'
 
 export const orgSchema = z.object({
   name: nameSchema,
+  org_id: selectOptionSchema(),
   description: descSchema,
 })
 
 export function CreateOrg() {
   const { t } = useTranslation()
 
-  const defaultComboboxOrgData = useDefaultCombobox('org')
-
   const { id: projectId } = storage.getProject()
 
-  const [filteredComboboxData, setFilteredComboboxData] = useState<
-    OrgMapType[]
-  >([])
-  const selectedOrgId =
-    filteredComboboxData.length !== 1 ? '' : filteredComboboxData[0]?.id
-
   const { mutate, isLoading, isSuccess } = useCreateOrg()
+
+  const orgListCache: OrgList | undefined = queryClient.getQueryData(['orgs'], {
+    exact: false,
+  })
+  const { acc: orgFlattenData } = flattenData(
+    orgListCache?.organizations || [],
+    ['id', 'name', 'level', 'description', 'parent_name'],
+    'sub_orgs',
+  )
+  const defaultComboboxOrgData = useDefaultCombobox('org')
+  const orgSelectOptions = [defaultComboboxOrgData, ...orgFlattenData]
 
   return (
     <FormDrawer
@@ -66,7 +69,9 @@ export function CreateOrg() {
           mutate({
             data: {
               project_id: projectId,
-              org_id: selectedOrgId,
+              org_id: (
+                values.org_id as unknown as { value: string; label: string }
+              ).value,
               name: values.name,
               description: values.description,
             },
@@ -74,21 +79,29 @@ export function CreateOrg() {
         }}
         schema={orgSchema}
       >
-        {({ register, formState }) => (
+        {({ register, formState, control }) => (
           <>
             <InputField
               label={t('cloud:org_manage.org_manage.add_org.name') ?? 'Name'}
               error={formState.errors['name']}
               registration={register('name')}
             />
-            <ComboBoxSelectOrg
-              label={
-                t('cloud:org_manage.org_manage.add_org.parent') ??
-                'Parent organization'
-              }
-              setFilteredComboboxData={setFilteredComboboxData}
-              hasDefaultComboboxData={defaultComboboxOrgData}
-            />
+            <div className="space-y-1">
+              <SelectDropdown
+                label={t('cloud:org_manage.device_manage.add_device.parent')}
+                name="org_id"
+                control={control}
+                options={
+                  orgSelectOptions?.map(org => ({
+                    label: org?.name,
+                    value: org?.id,
+                  })) || [{ label: t('loading:org'), value: '' }]
+                }
+              />
+              <p className="text-body-sm text-primary-400">
+                {formState?.errors?.org_id?.message}
+              </p>
+            </div>
             <TextAreaField
               label={
                 t('cloud:org_manage.org_manage.add_org.desc') ?? 'Description'
