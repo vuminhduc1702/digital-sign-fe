ef2b8094c39ef42bf9878d280f171988b6f4ef77
anhnt9 <anhnt9@rikkeisoft.com>
2023-11-06 18:37:04 +0700

    Fix bugs and update things service

--- src/cloud/customProtocol/components/CreateAdapter.tsx
+++ src/cloud/customProtocol/components/CreateAdapter.tsx
@@ -190,15 +190,7 @@ export const outputList = [
   {
     label: i18n.t('cloud:custom_protocol.service.bool'),
     value: 'bool',
-  },
-  {
-    label: i18n.t('cloud:custom_protocol.service.time'),
-    value: 'time',
-  },
-  {
-    label: i18n.t('cloud:custom_protocol.service.bin'),
-    value: 'bin',
-  },
+  }
 ]
 
 export const thingTypeList = [
