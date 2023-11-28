dd72939517e09ba0f79cd9b5e53669d1b9ac4f7b
nguyenlh6 <nguyenlehoang361@gmail.com>
2023-11-28 09:05:20 +0700

    fix attr

--- src/cloud/orgManagement/components/Attributes/AttrTable.tsx
+++ src/cloud/orgManagement/components/Attributes/AttrTable.tsx
@@ -60,7 +60,7 @@ function AttrTableContextMenu({
           />
         }
       >
-        <Menu.Items className="divide-secondary-400 absolute right-0 z-10 mt-6 w-40 origin-top-right divide-y rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <Menu.Items className="absolute right-0 z-10 mt-6 w-40 origin-top-right divide-y divide-secondary-400 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
           <div className="p-1">
             <MenuItem
               icon={
@@ -83,7 +83,7 @@ function AttrTableContextMenu({
               ).replace('{{ATTRNAME}}', attribute_key)}
               triggerButton={
                 <Button
-                  className="hover:text-primary-400 w-full justify-start border-none"
+                  className="w-full justify-start border-none hover:text-primary-400"
                   variant="trans"
                   size="square"
                   startIcon={
