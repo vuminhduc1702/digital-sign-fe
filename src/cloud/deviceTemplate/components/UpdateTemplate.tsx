7b88ecc3818dff4fd2399d19cd5990d413ade51f
nguyenlh6 <nguyenlehoang361@gmail.com>
2023-12-05 10:55:24 +0700

    fix

--- src/cloud/deviceTemplate/components/UpdateTemplate.tsx
+++ src/cloud/deviceTemplate/components/UpdateTemplate.tsx
@@ -60,7 +60,6 @@ export function UpdateTemplate({
     'id',
     'name',
   ])
-  console.log('RuleFlattenData: ', RuleFlattenData)
   const RuleSelectOptions = RuleFlattenData?.map(ruchains => ({
     label: ruchains?.name,
     value: JSON.parse(ruchains?.id)?.id,
@@ -72,21 +71,10 @@ export function UpdateTemplate({
     config: { suspense: false },
   })
 
-  const { register, formState, watch, handleSubmit, control } = useForm<
+  const { register, formState, watch, handleSubmit, control, reset } = useForm<
     UpdateTemplateDTO['data']
   >({
     resolver: templateAttrSchema && zodResolver(templateAttrSchema),
-    defaultValues: {
-      name: selectedUpdateTemplate?.name,
-      rule_chain_id: selectedUpdateTemplate?.rule_chain_id,
-      attributes:
-        attrData?.attributes.map((attribute: Attribute) => ({
-          attribute_key: attribute.attribute_key,
-          logged: attribute.logged,
-          value: attribute.value.toString(),
-          value_t: attribute.value_type,
-        })) || [],
-    },
   })
 
   const { fields, append, remove } = useFieldArray({
@@ -101,6 +89,24 @@ export function UpdateTemplate({
       close()
     }
   }, [isSuccess, close])
+
+  useEffect(() => {
+    if (attrData != null) {
+      reset({
+        name: selectedUpdateTemplate?.name,
+        rule_chain_id: selectedUpdateTemplate?.rule_chain_id,
+        attributes: attrData?.attributes.map((attribute: Attribute) => ({
+          attribute_key: attribute.attribute_key,
+          logged: attribute.logged,
+          value:
+            attribute.value != null && attribute.value !== ''
+              ? JSON.stringify(attribute.value)
+              : '',
+          value_t: attribute.value_type,
+        })),
+      })
+    }
+  }, [attrData, selectedUpdateTemplate])
 
   const showSpinner = useSpinDelay(attrLoading, {
     delay: 150,
@@ -215,6 +221,7 @@ export function UpdateTemplate({
                       `attributes.${index}.value_t` as const,
                     )}
                     options={valueTypeOptions}
+                    disabled
                   />
                   {watch(`attributes.${index}.value_t`) === 'BOOL' ? (
                     <SelectField
@@ -225,6 +232,7 @@ export function UpdateTemplate({
                         `attributes.${index}.value` as const,
                       )}
                       options={booleanSelectOption}
+                      disabled
                     />
                   ) : (
                     <InputField
@@ -240,10 +248,11 @@ export function UpdateTemplate({
                           ? 'number'
                           : 'text'
                       }
+                      disabled
                     />
                   )}
                   <FieldWrapper
-                    className="mt-2 w-fit space-y-2"
+                    className="w-fit space-y-2"
                     label={t('cloud:org_manage.org_manage.add_attr.logged')}
                     error={formState?.errors?.attributes?.[index]?.logged}
                   >
@@ -256,27 +265,13 @@ export function UpdateTemplate({
                             {...field}
                             checked={value}
                             onCheckedChange={onChange}
+                            disabled
                           />
                         )
                       }}
                     />
                   </FieldWrapper>
                 </div>
-
-                <Button
-                  type="button"
-                  size="square"
-                  variant="trans"
-                  className="mt-10 self-start border-none"
-                  onClick={() => remove(index)}
-                  startIcon={
-                    <img
-                      src={btnDeleteIcon}
-                      alt="Delete device template"
-                      className="h-8 w-8"
-                    />
-                  }
-                />
               </section>
             ))}
           </>
