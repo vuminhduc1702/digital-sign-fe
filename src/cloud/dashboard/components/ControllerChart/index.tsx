745c0f2cef37bb77affde18cbfd2fa034e9e2d73
nguyenlh6 <nguyenlehoang361@gmail.com>
2023-12-12 18:40:32 +0700

    fix db disconnect ws

--- src/cloud/dashboard/components/ControllerChart/index.tsx
+++ src/cloud/dashboard/components/ControllerChart/index.tsx
@@ -1,43 +1,49 @@
-import { type WebSocketMessage } from 'react-use-websocket/dist/lib/types'
-import { useCallback, useEffect } from 'react'
+import { useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
 
-import { useWS } from '~/utils/hooks'
-import { WEBSOCKET_URL } from '../../routes/DashboardDetail'
 import storage from '~/utils/storage'
 import { useNotificationStore } from '~/stores/notifications'
 
-import { type BaseWSRes } from '~/types'
+import { type SendMessage } from 'react-use-websocket/dist/lib/types'
+import { type DashboardWS } from '../../types'
 
-type ControllerWSRes = {
-  data: {
-    code: 0 | 1
-    message: string
-    data?: any
-  }
-} & BaseWSRes
-
-export function ControllerButton(data: { data: string }) {
+export function ControllerButton({
+  data,
+  sendMessage,
+  lastJsonMessage,
+}: {
+  data: string
+  sendMessage: SendMessage
+  lastJsonMessage: DashboardWS
+}) {
   const { t } = useTranslation()
   const { addNotification } = useNotificationStore()
 
   const projectId = storage.getProject()?.id
 
-  const [{ sendMessage, lastJsonMessage, readyState }, connectionStatus] =
-    useWS<ControllerWSRes>(WEBSOCKET_URL)
-  // console.log('lastJsonMessage controller', lastJsonMessage)
+  const { input, service_name, thing_id } = JSON.parse(data).executorCmds[0]
 
-  const handleSendMessage = useCallback(
-    (message: WebSocketMessage) => sendMessage(message),
-    [],
-  )
-
-  const { input, service_name, thing_id } = JSON.parse(Object.values(data)[0])
-    .executorCmds[0]
+  function handleSendMessage() {
+    sendMessage(
+      JSON.stringify({
+        executorCmds: [
+          {
+            project_id: projectId,
+            thing_id,
+            service_name,
+            input,
+          },
+        ],
+      }),
+    )
+  }
 
   useEffect(() => {
     if (lastJsonMessage != null) {
-      if (lastJsonMessage?.errorCode === 0) {
+      if (
+        lastJsonMessage?.errorCode === 0 &&
+        !Array.isArray(lastJsonMessage.data)
+      ) {
         addNotification({
           type: 'success',
           title: t(
@@ -54,20 +60,7 @@ export function ControllerButton(data: { data: string }) {
     <div className="relative h-full">
       <div
         className="absolute left-[50%] top-1/2 z-10 mt-[35px] h-[110px] w-[120px] -translate-x-1/2 -translate-y-1/2 cursor-pointer rounded-full border-b-[30px] border-solid border-[#990033] bg-[#ff0066] p-0 transition-all active:mt-[55px] active:h-[85px] active:rounded-[100px] active:border-b-0"
-        onClick={() =>
-          handleSendMessage(
-            JSON.stringify({
-              executorCmds: [
-                {
-                  project_id: projectId,
-                  thing_id,
-                  service_name,
-                  input,
-                },
-              ],
-            }),
-          )
-        }
+        onClick={handleSendMessage}
       >
         <p className="absolute left-1/2 top-1/2 -translate-x-1/2 text-body-xs text-white">
           {service_name}
