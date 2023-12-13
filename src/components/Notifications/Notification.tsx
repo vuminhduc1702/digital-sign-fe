181e05f052f5a83587e5990620b62884281c4ff5
bmq420 <buiminhquan_t65@hus.edu.vn>
2023-12-13 10:47:03 +0700

    fix mqtt adapter

--- src/components/Notifications/Notification.tsx
+++ src/components/Notifications/Notification.tsx
@@ -50,52 +50,96 @@ export const Notification = ({
   })
 
   return (
-    <Dialog
-      as="div"
-      static
-      className="fixed inset-0 z-50 overflow-hidden flex flex-col items-end space-y-4 px-4 py-6 sm:items-start sm:p-6"
-      open={true}
-      onClose={() => {
-        onDismiss(id)
-      }}
-    >
-      <div className="flex w-full flex-col items-center space-y-4 sm:items-end z-50">
-        <Transition
-          show={true}
-          as={Fragment}
-          enter="transform ease-out duration-300 transition"
-          enterFrom="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
-          enterTo="translate-y-0 opacity-100 sm:translate-x-0"
-          leave="transition ease-in duration-100"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
+    <>
+      {title === 'MQTT connection không tồn tại' ? (
+        <Dialog
+          as="div"
+          static
+          className="fixed inset-0 z-50 overflow-hidden flex flex-col items-end space-y-4 px-4 py-6 sm:items-start sm:p-6"
+          open={true}
+          onClose={() => {
+            onDismiss(id)
+          }}
         >
-          <div className="pointer-events-auto w-full max-w-sm overflow-hidden rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
-            <div className="p-4" role="alert" aria-label={title}>
-              <div className="flex items-start">
-                <div className="flex-shrink-0">{icons[type]}</div>
-                <div className="ml-3 w-0 flex-1 pt-0.5">
-                  <p className="text-body-sm font-medium text-gray-900">
-                    {title}
-                  </p>
-                  <p className="mt-1 text-body-sm text-gray-500">{message}</p>
+          <div className="flex w-full flex-col items-center space-y-4 sm:items-end z-50">
+            <Transition
+              show={true}
+              as={Fragment}
+              enter="transform ease-out duration-300 transition"
+              enterFrom="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
+              enterTo="translate-y-0 opacity-100 sm:translate-x-0"
+              leave="transition ease-in duration-100"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <div className="pointer-events-auto w-full max-w-sm overflow-hidden rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
+                <div className="p-4" role="alert" aria-label={title}>
+                  <div className="flex items-start">
+                    <div className="flex-shrink-0">{icons[type]}</div>
+                    <div className="ml-3 w-0 flex-1 pt-0.5">
+                      <p className="text-body-sm font-medium text-gray-900">
+                        {title}
+                      </p>
+                      <p className="mt-1 text-body-sm text-gray-500">
+                        {message}
+                      </p>
+                    </div>
+                    <div className="ml-4 flex flex-shrink-0">
+                      <button
+                        className="inline-flex rounded-md bg-white text-secondary-900 hover:text-secondary-700 focus:outline-none focus:ring-2 focus:ring-secondary-600 focus:ring-offset-2"
+                        onClick={() => {
+                          onDismiss(id)
+                        }}
+                      >
+                        <span className="sr-only">Close</span>
+                        <XMarkIcon className="h-5 w-5" aria-hidden="true" />
+                      </button>
+                    </div>
+                  </div>
                 </div>
-                <div className="ml-4 flex flex-shrink-0">
-                  <button
-                    className="inline-flex rounded-md bg-white text-secondary-900 hover:text-secondary-700 focus:outline-none focus:ring-2 focus:ring-secondary-600 focus:ring-offset-2"
-                    onClick={() => {
-                      onDismiss(id)
-                    }}
-                  >
-                    <span className="sr-only">Close</span>
-                    <XMarkIcon className="h-5 w-5" aria-hidden="true" />
-                  </button>
+              </div>
+            </Transition>
+          </div>
+        </Dialog>
+      ) : (
+        <div className="flex w-full flex-col items-center space-y-4 sm:items-end z-50">
+          <Transition
+            show={true}
+            as={Fragment}
+            enter="transform ease-out duration-300 transition"
+            enterFrom="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
+            enterTo="translate-y-0 opacity-100 sm:translate-x-0"
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="pointer-events-auto w-full max-w-sm overflow-hidden rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
+              <div className="p-4" role="alert" aria-label={title}>
+                <div className="flex items-start">
+                  <div className="flex-shrink-0">{icons[type]}</div>
+                  <div className="ml-3 w-0 flex-1 pt-0.5">
+                    <p className="text-body-sm font-medium text-gray-900">
+                      {title}
+                    </p>
+                    <p className="mt-1 text-body-sm text-gray-500">{message}</p>
+                  </div>
+                  <div className="ml-4 flex flex-shrink-0">
+                    <button
+                      className="inline-flex rounded-md bg-white text-secondary-900 hover:text-secondary-700 focus:outline-none focus:ring-2 focus:ring-secondary-600 focus:ring-offset-2"
+                      onClick={() => {
+                        onDismiss(id)
+                      }}
+                    >
+                      <span className="sr-only">Close</span>
+                      <XMarkIcon className="h-5 w-5" aria-hidden="true" />
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </Transition>
-      </div>
-    </Dialog>
+          </Transition>
+        </div>
+      )}
+    </>
   )
 }
