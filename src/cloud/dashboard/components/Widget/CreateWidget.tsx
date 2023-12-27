da908fc82fd3c71af208f05556d3cfdb2b221c35
anhnt9 <anhnt9@rikkeisoft.com>
2023-12-27 18:03:13 +0700

    Update dashboard

--- src/cloud/dashboard/components/Widget/CreateWidget.tsx
+++ src/cloud/dashboard/components/Widget/CreateWidget.tsx
@@ -455,7 +455,14 @@ export function CreateWidget({
     mutate: attrChartMutate,
     isLoading: attrChartIsLoading,
   } = useCreateAttrChart()
+<<<<<<< HEAD
   const attrSelectData = attrChartData?.entities?.flatMap(item => item.attr_keys).map(key => ({ label: key, value: key }))
+=======
+  const attrSelectData = attrChartData?.keys?.map(item => ({
+    value: item,
+    label: item,
+  }))
+>>>>>>> e1112bc... Update dashboard
   const attrSelectDataForMap = [
     { value: 'lat', label: 'latitude' },
     { value: 'long', label: 'longitude' }
@@ -880,6 +887,7 @@ export function CreateWidget({
                             />
                           )
                         }
+<<<<<<< HEAD
                         {/* <InputField
                           label={t('cloud:dashboard.config_chart.label')}
                           error={
@@ -898,6 +906,8 @@ export function CreateWidget({
                             setInputField(temp)
                           }}
                         />
+=======
+>>>>>>> e1112bc... Update dashboard
                         {/* <InputField
                           label={t('cloud:dashboard.config_chart.label')}
                           error={
