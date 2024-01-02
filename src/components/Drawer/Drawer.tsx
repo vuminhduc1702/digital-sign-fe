61dceeb04971b574b99bbf3d1e7e8d3fe081f839
bmq420 <buiminhquan_t65@hus.edu.vn>
2024-01-02 21:24:40 +0700

    fix drawer

--- src/components/Drawer/Drawer.tsx
+++ src/components/Drawer/Drawer.tsx
@@ -21,6 +21,16 @@ export type DrawerProps = {
   size?: keyof typeof sizes
 }
 
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+  SheetPortal,
+} from '../Sheet'
+
 export const Drawer = ({
   title,
   children,
@@ -30,35 +40,24 @@ export const Drawer = ({
   size = 'md',
 }: DrawerProps) => {
   return (
-    <Transition.Root show={isOpen} as={Fragment}>
-      <div
-        // as="div"
-        // static
-        className="fixed inset-0 z-40 overflow-hidden"
-        // open={isOpen}
-        // onClose={() => null}
-      >
+    <SheetContent
+      onInteractOutside={e => {
+        e.preventDefault()
+      }}
+    >
+      <div className="fixed inset-0 z-40 overflow-hidden">
         <div className="absolute inset-0 overflow-hidden">
-          <div className="absolute inset-0" />
-          <div className="fixed inset-y-0 right-0 flex max-w-full pl-10">
-            <Transition.Child
-              as={Fragment}
-              enter="transform transition ease-in-out duration-300 sm:duration-300"
-              enterFrom="translate-x-full"
-              enterTo="translate-x-0"
-              leave="transform transition ease-in-out duration-300 sm:duration-300"
-              leaveFrom="translate-x-0"
-              leaveTo="translate-x-full"
-            >
-              <div className={clsx('w-screen', sizes[size])}>
-                <div className="flex h-full flex-col divide-y divide-gray-200 bg-white shadow-xl">
-                  <div className="flex min-h-0 flex-1 flex-col overflow-y-scroll py-6">
-                    <div className="px-4 sm:px-6">
-                      <div className="flex items-start justify-between">
-                        <div className="text-body-md font-medium text-gray-900">
-                          {title}
-                        </div>
-                        <div className="ml-3 flex h-7 items-center">
+          <div className="fixed inset-y-0 right-0 flex max-w-full">
+            <div className={clsx('w-screen', sizes[size])}>
+              <div className="flex h-full flex-col divide-y divide-gray-200 bg-white shadow-xl">
+                <div className="flex min-h-0 flex-1 flex-col overflow-y-scroll py-6">
+                  <div className="px-4 sm:px-6">
+                    <div className="flex items-start justify-between">
+                      <div className="text-body-md font-medium text-gray-900">
+                        {title}
+                      </div>
+                      <div className="ml-3 flex h-7 items-center">
+                        <SheetTrigger>
                           <button
                             className="rounded-md bg-white text-secondary-900 hover:text-secondary-700 focus:outline-none focus:ring-2 focus:ring-secondary-600"
                             onClick={onClose}
@@ -66,22 +65,22 @@ export const Drawer = ({
                             <span className="sr-only">Close panel</span>
                             <XMarkIcon className="h-6 w-6" aria-hidden="true" />
                           </button>
-                        </div>
+                        </SheetTrigger>
                       </div>
                     </div>
-                    <div className="relative mt-6 flex grow justify-center px-4 sm:px-6">
-                      {children}
-                    </div>
                   </div>
-                  <div className="flex flex-shrink-0 justify-end space-x-2 p-4">
-                    {renderFooter()}
+                  <div className="relative mt-6 flex grow justify-center px-4 sm:px-6">
+                    {children}
                   </div>
                 </div>
+                <div className="flex flex-shrink-0 justify-end space-x-2 p-4">
+                  {renderFooter()}
+                </div>
               </div>
-            </Transition.Child>
+            </div>
           </div>
         </div>
       </div>
-    </Transition.Root>
+    </SheetContent>
   )
 }
