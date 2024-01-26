a46aa23e84196185de1ac348b59751b4cb128062
khiemtn2 <khiemtn2@viettel.com.vn>
2024-01-26 14:46:00 +0700

    khiemtn2-lwm2m

--- src/cloud/orgManagement/routes/OrgMap.tsx
+++ src/cloud/orgManagement/routes/OrgMap.tsx
@@ -1,74 +1,76 @@
-import React from 'react';
-import { Tree, RenderCustomNodeElementFn } from 'react-d3-tree';
-import storage from '~/utils/storage';
-import { useProjectById } from '~/cloud/project/api';
-import { useGetOrgs } from '~/layout/MainLayout/api';
-import { type Org } from '~/layout/MainLayout/types';
-import '~/style/treeComponent.css';
-
+import Tooltip from "rc-tooltip"
+import "rc-tooltip/assets/bootstrap.css"
+import { RenderCustomNodeElementFn, Tree } from 'react-d3-tree'
+import { useNavigate } from 'react-router-dom'
+import { useProjectById } from '~/cloud/project/api'
+import { useGetOrgs } from '~/layout/MainLayout/api'
+import { type Org } from '~/layout/MainLayout/types'
+import { PATHS } from '~/routes/PATHS'
+import '~/style/treeComponent.css'
+import storage from '~/utils/storage'
 export function OrgMap() {
-  const projectId = storage.getProject()?.id;
+  const projectId = storage.getProject()?.id
+  const navigate = useNavigate()
   const { data: projectByIdData } = useProjectById({
     projectId,
     config: { enabled: !!projectId },
-  });
+  })
   const { data: orgData } = useGetOrgs({ projectId });
-
+  
   function convertOrgToTree(org: Org): any {
     return {
       name: org.name,
-      attributes: { level: org.level },
+      attributes: { id:org.id, level: org.level, description:org.description },
       children: org.sub_orgs ? org.sub_orgs.map(convertOrgToTree) : [],
     }
   }
+  const handleTextClick = (nodeDatum) => {
+    return navigate(`${PATHS.ORG_MANAGE}/${projectId}/${nodeDatum?.attributes.id}`)
+  }
+  const renderRectSvgNode: RenderCustomNodeElementFn = ({ nodeDatum, toggleNode, foreignObjectProps={} }) => {
+    const levelString: string | undefined = nodeDatum.attributes?.level?.toString()
+    const hasChildren = nodeDatum.children?.length
+    const isNodeExpanded = nodeDatum.__rd3t.collapsed !== true;
 
-  // const renderCustomNode: RenderCustomNodeElementFn = ({
-  //   nodeDatum,
-  //   hierarchyPointNode,
-  //   toggleNode,
-  //   onNodeClick,
-  //   onNodeMouseOver,
-  //   onNodeMouseOut,
-  //   addChildren,
-  // }) => {
-  //   // Thực hiện việc tạo ra phần tử JSX của node theo ý muốn
-  //   const rectWidth = 150;
-  //   const rectHeight = 50;
-  //   return (
-  //     <g>
-  //       {/* Hình chữ nhật */}
-  //       <rect
-  //         width={rectWidth}
-  //         height={rectHeight}
-  //         fill=""
-  //         onClick={toggleNode}
-  //         onMouseOver={onNodeMouseOver}
-  //         onMouseOut={onNodeMouseOut}
-  //       />
-  //       {/* Hiển thị thông tin bên trong hình chữ nhật */}
-  //       {/* <div className="node-text"> */}
-  //         <text x={rectWidth / 2} y={rectHeight / 2} dominantBaseline="middle" textAnchor="middle" fill="white">
-  //           {nodeDatum.name}
-  //         </text>
-  //       {/* </div> */}
-  //       {/* Thêm bất kỳ phần tử JSX hoặc logic xử lý tại đây */}
-  //     </g>
-  //   );
-  // };
-  const renderRectSvgNode: RenderCustomNodeElementFn =({ nodeDatum, toggleNode }) => (
-    
-    <g>
-      <rect fill="pink" width="140" height="80" x="-70" y="-40" onClick={toggleNode} />
-      <text fill="black" strokeWidth="1" x="-45" y="-10" fontFamily="" fontWeight="normal" fontSize="20px">
-        {nodeDatum.name}
-      </text>
-      {/* {nodeDatum.attributes?.department && (
-        <text fill="black" x="20" dy="20" strokeWidth="1">
-          Department: {nodeDatum.attributes?.department}
+    const handleToggleNode = () => {
+    toggleNode();
+  };
+    return (
+      <g className={`custom-node custom-node-${levelString || '0'}`}>
+        <rect width="140" height="80" x="-70" y="-40" />
+        <Tooltip
+        placement="rightBottom"
+        overlay={
+          <div>
+            {/* <p>ID:  {nodeDatum.attributes?.id}</p> */}
+            <p>Tên:  {nodeDatum.name}</p>
+            <p>Mô tả:  {nodeDatum.attributes?.description}</p>
+          </div>
+        }
+        >
+        <text 
+          fill="black"
+          strokeWidth="1" x="0" y="0" 
+          fontWeight="normal" 
+          fontSize="15px" 
+          text-anchor="middle" 
+          dominant-baseline="middle"
+          onClick={() => {handleTextClick(nodeDatum)}}
+        >
+          {nodeDatum.name.length > 9 ? `${nodeDatum.name.slice(0, 9)}...` : nodeDatum.name}
         </text>
-      )} */}
-    </g>
-  )
+        </Tooltip>
+        {hasChildren ? (
+          <g>
+            <circle cx={0} cy={40} r={12} fill="white" stroke="black" strokeWidth={1} onClick={handleToggleNode} style={{ cursor: "pointer" }} />
+            <text x={0} y={40} fontSize="15px" textAnchor="middle" dy=".3em" onClick={handleToggleNode} style={{ cursor: "pointer"}}>
+              {isNodeExpanded ? '-' : '+'}
+            </text>
+          </g>
+        ) : null}
+      </g>
+    )
+  }
   const treeData: any = {
     name: projectByIdData?.name || 'Default Project',
     children: orgData?.organizations.map(convertOrgToTree),
@@ -80,14 +82,14 @@ export function OrgMap() {
         orientation="vertical"
         pathFunc="step"
         nodeSize={{ x: 80, y: 150 }}
-        svgClassName="custom-svg-class"
-        translate={{ x: 700, y: 150 }}
+        svgClassName={`custom-svg-class-0`}
+        translate={{ x: 600, y: 150 }}
         scaleExtent={{ min: 0.5, max: 2 }}
-        rootNodeClassName="node__root"
-        branchNodeClassName="node__branch"
-        leafNodeClassName="node__leaf"
         separation={{ siblings: 2, nonSiblings: 2 }}
-        renderCustomNodeElement={renderRectSvgNode}
+        renderCustomNodeElement={(rd3tProps) =>
+          renderRectSvgNode({ ...rd3tProps })
+        }
+        initialDepth={1}
       />
     </div>
   )
