1e9d610edddd9816e919fa2adb4ae90cbd1b78a8
Tle123-tt <leminhthuc12a0@gmail.com>
2024-01-29 14:24:40 +0700

    update and Fix

--- src/pages/LandingPage/components/section-order.tsx
+++ src/pages/LandingPage/components/section-order.tsx
@@ -32,7 +32,7 @@ export function SectionOrder() {
               <div className="w-[373px] text-[35px] leading-[43px] text-white">
                 {t('landingpage:order.manage_platform')}
               </div>
-              <div className="pt-5 text-[16px] leading-[20.8px] text-white">
+              {/* <div className="pt-5 text-[16px] leading-[20.8px] text-white">
                 Hệ thống Cloud Innoway của Viettel cho phép các nhà tích hợp hệ
                 thống triển khai tích hợp các giải pháp quản lý điều khiển thiết
                 bị một cách hiệu quả
@@ -82,6 +82,23 @@ export function SectionOrder() {
                     </p>
                   </div>
                 </div>
+              </div> */}
+              <div className="pt-5 text-[16px] leading-[20.8px] text-white">
+                Nền tảng của chúng tôi đã hỗ trợ rút ngắn thời gian phát triển
+                các giải pháp IoT của Viettel tới 70%. Tiết kiệm chi phí vận
+                hành tới 80%.
+              </div>
+              <div className="pt-4 text-[16px] leading-[20.8px] text-white">
+                Nếu các bạn có 1 ý tưởng cung cấp dịch vụ IoT xuất sắc, các bạn
+                muốn thử nghiệm nhanh? chúng tôi sẽ giúp bạn miễn phí (dưới 100
+                thiết bị)
+              </div>
+              <div className="pt-4 text-[16px] leading-[20.8px] text-white">
+                Nếu các bạn đang triển khai 1 dự án đầu tư công, mà các nhà cung
+                cấp nền tảng nước ngoài không thể đáp ứng yêu cầu? Nền tảng của
+                chúng tôi phát triển hoàn toàn bởi đội ngũ Viettel, hỗ trợ tùy
+                biến không giới hạn, chúng tôi sẽ đồng hành cùng các bạn trọn
+                vòng đời sản phẩm.
               </div>
               <div className="flex pt-8">
                 <Button
@@ -101,10 +118,10 @@ export function SectionOrder() {
                 >
                   {t('landingpage:experience_now')}
                 </Button>
-                <Link to='https://innoway.gitbook.io/innoway/' target="_blank">
+                <Link to="https://innoway.gitbook.io/innoway/" target="_blank">
                   <Button
                     type="button"
-                    className="rounded-r-lg rounded-tl-lg border-none ml-7 bg-white text-[#EA0033] hover:-translate-y-px hover:opacity-100 hover:shadow-xl"
+                    className="ml-7 rounded-r-lg rounded-tl-lg border-none bg-white text-[#EA0033] hover:-translate-y-px hover:opacity-100 hover:shadow-xl"
                     variant="primary"
                     endIcon={
                       <ArrowRightUpLine
