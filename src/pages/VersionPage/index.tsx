27f3af9f68cd3ffcc2fceb6360e716e652f12513
nguyenlh6 <nguyenlh6@viettel.com.vn>
2024-02-06 16:47:19 +0700

    0.2.3

--- src/pages/VersionPage/index.tsx
+++ src/pages/VersionPage/index.tsx
@@ -3,8 +3,8 @@ export function VersionPage() {
     Name: 'IoT Platform',
     Organization:
       'Viettel High Technology Industries Corporation - Viettel Group',
-    Version: '0.2.2',
-    'Build Date': '29/01/2024',
+    Version: '0.2.3',
+    'Build Date': '06/02/2024',
   }
   const versionJson = JSON.stringify(version, null, 2)
   return <div>{versionJson}</div>
