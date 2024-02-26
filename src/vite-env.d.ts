b4acc2cde1400a7afc67158f175f44170cebb5b0
nguyenlh6 <nguyenlh6@viettel.com.vn>
2024-02-26 17:32:10 +0700

    auto version page

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string
+  readonly VITE_FLOW_URL: string
+  readonly VITE_WS_URL: string
+  readonly VITE_APP_VERSION: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
