e0f424350b1dfd3c4fa25b728588e238c8236dee
nguyenlh6 <nguyenlh6@viettel.com.vn>
2024-02-28 08:37:39 +0700

    add script package.json, lint stage

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  '*.{js,jsx,ts,tsx}': ['eslint --fix', 'eslint'],
-  // '**/*.ts?(x)': () => 'npm run type-check',
-  '*.{js,jsx,ts,tsx,css,md,json,yaml}': ['prettier --write'],
+  '/*.{js,mjs,ts,tsx}': ['eslint --fix', 'prettier --check --write'],
+  '/*.css': ['stylelint --allow-empty-input', 'prettier --write'],
+  '/*.{json,yml}': ['prettier --check --write'],
 }
