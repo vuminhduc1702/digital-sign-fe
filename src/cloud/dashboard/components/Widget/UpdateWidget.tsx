4a0ca22ae7069568d6d5311d6803964cde22b865
bmq420 <buiminhquan_t65@hus.edu.vn>
2024-05-24 18:10:48 +0700

    add dashboard

--- src/cloud/dashboard/components/Widget/UpdateWidget.tsx
+++ src/cloud/dashboard/components/Widget/UpdateWidget.tsx
@@ -356,9 +356,6 @@ export function UpdateWidget({
     reset()
   }
 
-  console.log(formState.errors)
-  console.log(watch())
-
   return (
     <FormDialog
       size="max"
@@ -380,7 +377,7 @@ export function UpdateWidget({
                       : t('cloud:dashboard.config_chart.update')
       }
       isDone={isDone}
-      resetForm={resetForm}
+      resetData={resetForm}
       body={
         <Form {...form}>
           <form
@@ -894,9 +891,14 @@ export function UpdateWidget({
                             }
                           />
                         )}
-                        {!['GAUGE', 'TABLE', 'MAP', 'CONTROLLER', 'CARD'].find(
-                          e => widgetInfoMemo?.description === e,
-                        ) ? (
+                        {![
+                          'GAUGE',
+                          'TABLE',
+                          'MAP',
+                          'CONTROLLER',
+                          'CONTROLLER_FORM',
+                          'CARD',
+                        ].find(e => widgetInfoMemo?.description === e) ? (
                           <div className="space-y-1">
                             <FieldWrapper
                               label={t('cloud:dashboard.config_chart.color')}
